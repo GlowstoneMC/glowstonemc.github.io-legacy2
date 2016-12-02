247f60cc2883715154c61180cf762cb9a3bcdb59
momothereal <momothereal.mc@gmail.com>
2016-12-02 16:24:49 -0500

    Un-_blank, recent announcements banner

--- js/news-forums.js
+++ js/news-forums.js
@@ -1,31 +1,34 @@
-$(function() {
+function loadNews() {
   // Get the data from the forums
   var topicArray = {};
   $.getJSON(getURL("https://forums.glowstone.net/api/category/12/announcements"), function(data) {
-    var topics = data.topics;
-    var length = topics.length > 5 ? 5 : topics.length;
-    for (var i = 0; i < length; i++) {
+    var topics = [];
+    var index = 0;
+    for (var i = 0; i < data.topics.length; i++) {
+      if (data.topics[i].deleted) {
+        continue;
+      }
+      topics[index++] = data.topics[i];
+    }
+    for (var i = 0; i < 5; i++) {
       var t = topics[i];
       var html = "";
-      if (!t.deleted) {
-        var api = getURL("https://forums.glowstone.net/api/topic/" + t.slug);
-        $.ajax({
-          dataType: "json",
-          url: api,
-          async: false,
-          success: function(topic) {
-            var topicurl = "https://forums.glowstone.net/topic/" + topic.slug;
-            var timestamp = Number(topic.timestamp);
-            var date = timeDifference(new Date().getTime(), timestamp);
-            var author = topic.posts[0].user.username;
-            var comments = topic.posts.length - 1;
-            var content = topic.posts[0].content.replace("\\n", "");
-            var title = topic.title;
-            topicArray['<div class="content box"><h2>' + title + '<a class="button is-outlined" style="float: right" href="' + topicurl + '">View post</a><br><h5>Posted by ' + author + ' <strong id="datetest">' + date + '</strong>, ' + comments + ' comment' + (comments <= 1 ? "" : "s") + '</h5></h2>' + content + '</div>'] = timestamp;
-        }});
-      }
+      var api = getURL("https://forums.glowstone.net/api/topic/" + t.slug);
+      $.ajax({
+        dataType: "json",
+        url: api,
+        async: false,
+        success: function(topic) {
+          var topicurl = "https://forums.glowstone.net/topic/" + topic.slug;
+          var timestamp = Number(topic.timestamp);
+          var date = timeDifference(new Date().getTime(), timestamp);
+          var author = topic.posts[0].user.username;
+          var comments = topic.posts.length - 1;
+          var content = topic.posts[0].content.replace("\\n", "");
+          var title = topic.title;
+          topicArray['<div class="content box"><h2>' + title + '<a class="button is-outlined" style="float: right" href="' + topicurl + '">View post</a><br><h5>Posted by ' + author + ' <strong id="datetest">' + date + '</strong>, ' + comments + ' comment' + (comments === 1 ? "" : "s") + '</h5></h2>' + content + '</div>'] = timestamp;
+      }});
     }
-
   }).complete(function() {
     $("#announcements-loading").remove();
     for (var html in topicArray) {
@@ -33,4 +36,26 @@ $(function() {
     }
     $("#announcements-more").show();
   });
-});
+}
+
+function loadAnnouncementsFront(count) {
+  $.getJSON(getURL("https://forums.glowstone.net/api/category/12/announcements"), function(data) {
+    var topics = [];
+    var index = 0;
+    for (var i = 0; i < data.topics.length; i++) {
+      if (data.topics[i].deleted) {
+        continue;
+      }
+      topics[index++] = data.topics[i];
+    }
+    var build = "";
+    for (var i = 0; i < count; i++) {
+      build += '<a class="announcement-link" href="https://forums.glowstone.net/topic/' + topics[i].slug + '">' + topics[i].title + '</a>';
+      if (i < (count - 1)) {
+        build += ", ";
+      }
+    }
+    $("#announcements-titles").html(build);
+    $("#announcements-front").slideDown(500);
+  });
+}
