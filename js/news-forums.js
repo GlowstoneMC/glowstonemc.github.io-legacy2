14d778b72f99397836fc780af212e35d0248e226
momothereal <momothereal.mc@gmail.com>
2016-12-02 13:46:29 -0500

    Change some news display

--- js/news-forums.js
+++ js/news-forums.js
@@ -3,7 +3,8 @@ $(function() {
   var topicArray = {};
   $.getJSON(getURL("https://forums.glowstone.net/api/category/12/announcements"), function(data) {
     var topics = data.topics;
-    for (var i = 0; i < topics.length; i++) {
+    var length = topics.length > 5 ? 5 : topics.length;
+    for (var i = 0; i < length; i++) {
       var t = topics[i];
       var html = "";
       if (!t.deleted) {
@@ -20,7 +21,7 @@ $(function() {
             var comments = topic.posts.length - 1;
             var content = topic.posts[0].content.replace("\\n", "");
             var title = topic.title;
-            topicArray['<div class="content box"><h2>' + title + ' <div style="float: right; color: gray">' + comments + ' <span class="fa fa-comment" style="margin-top: 1px;"></span></div><br><h5><a href="' + topicurl + '">Posted</a> by ' + author + ' <strong id="datetest">' + date + '</strong></h5></h2>' + content + '</div>'] = timestamp;
+            topicArray['<div class="content box"><h2>' + title + '<a class="button is-outlined" style="float: right" href="' + topicurl + '">View post</a><br><h5>Posted by ' + author + ' <strong id="datetest">' + date + '</strong>, ' + comments + ' comment' + (comments <= 1 ? "" : "s") + '</h5></h2>' + content + '</div>'] = timestamp;
         }});
       }
     }
@@ -30,5 +31,6 @@ $(function() {
     for (var html in topicArray) {
       $("#announcements-parent").html($("#announcements-parent").html() + "<br>" + html);
     }
+    $("#announcements-more").show();
   });
 });
