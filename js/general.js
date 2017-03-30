b551fd66215675dc137cb128f18b04f75b8a5fca
momothereal <momothereal.mc@gmail.com>
2017-03-30 19:50:04 -0400

    Snapshot download, Download warning

--- js/general.js
+++ js/general.js
@@ -19,4 +19,17 @@ $(document).ready(function(){
   $("#chatModalBack").on('click', function(event){
     $("#chatModal").fadeOut(300);
   });
+
+  $("#snapshot").on('click', function(event) {
+    $("#snapshotModal").fadeIn(400);
+  });
+  $("#snapshotModalClose").on('click', function(event){
+    $("#snapshotModal").fadeOut(300);
+  });
+  $("#snapshotModalClose2").on('click', function(event){
+    $("#snapshotModal").fadeOut(300);
+  });
+  $("#snapshotModalBack").on('click', function(event){
+    $("#snapshotModal").fadeOut(300);
+  });
 });
